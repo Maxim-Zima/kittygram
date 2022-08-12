48d9b2d78e7b0593942d603f08a02d83c1191c58
Maxim Zimakov <maximum_zima@mail.ru>
2022-08-12 22:01:01 +0300

    2

--- cats/views.py
+++ cats/views.py
@@ -1,6 +1,10 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.generics import (ListCreateAPIView,
+                                     RetrieveUpdateDestroyAPIView)
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 
 from .models import Cat
 from .serializers import CatSerializer
@@ -17,3 +21,35 @@ def cat_list(request):
     cats = Cat.objects.all()
     serializer = CatSerializer(cats, many=True)
     return Response(serializer.data)
+
+
+class MyAPIView(APIView):
+    def get(self, request):
+        cats = Cat.objects.all()
+        serializer = CatSerializer(cats, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = CatSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CatList(ListCreateAPIView):
+    '''возвращает всю коллекцию котиков или создает новую запись в БД'''
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
+
+class CatDetail(RetrieveUpdateDestroyAPIView):
+    '''возвращает, обновляет или удаляет объекты модели по одном '''
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
+
+class CatViewSet(ModelViewSet):
+    '''вьюсет реализует все 6 операций CRUD для модели Cat'''
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,9 +1,15 @@
 from django.urls import include, path
+from rest_framework.routers import SimpleRouter
 
-from cats.views import cat_list
+from cats.views import CatDetail, CatList, CatViewSet
 
 urlpatterns = [
-   path('cats/', cat_list),
+   path('cats/', CatList.as_view()),
+   path('cats/<int:pk>', CatDetail.as_view()),
+   ]
+
+router = SimpleRouter()
+router.register('cats', CatViewSet)
+
+urlpatterns =  [path('', include(router.urls)),
 ]
-
-
